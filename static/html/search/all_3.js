c1a7c0716544b1d2d3002d06602e023bd58c4859
C3NZ <cenz@cenz.io>
2020-05-25 21:27:07 -0700

    [add] yet another test for the documentation output.

--- static/html/search/all_3.js
+++ static/html/search/all_3.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['engine_5fapi',['ENGINE_API',['../Core_8h.html#a0697ee40f69dc5dab56d13131f4c4835',1,'Core.h']]],
   ['engine_5fdebug_5fbreak',['ENGINE_DEBUG_BREAK',['../Core_8h.html#ae79feff140f19bd881e457bfa4cbfbd4',1,'Core.h']]],
+  ['engine_5fenable_5fasserts',['ENGINE_ENABLE_ASSERTS',['../Core_8h.html#a9b3ffcf9f4871788acdf12794eca48cf',1,'Core.h']]],
   ['event',['Event',['../classengine_1_1events_1_1Event.html',1,'engine::events']]],
   ['eventdispatcher',['EventDispatcher',['../classengine_1_1events_1_1EventDispatcher.html',1,'engine::events']]]
 ];
--- static/html/search/defines_1.js
+++ static/html/search/defines_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['engine_5fapi',['ENGINE_API',['../Core_8h.html#a0697ee40f69dc5dab56d13131f4c4835',1,'Core.h']]],
-  ['engine_5fdebug_5fbreak',['ENGINE_DEBUG_BREAK',['../Core_8h.html#ae79feff140f19bd881e457bfa4cbfbd4',1,'Core.h']]]
+  ['engine_5fdebug_5fbreak',['ENGINE_DEBUG_BREAK',['../Core_8h.html#ae79feff140f19bd881e457bfa4cbfbd4',1,'Core.h']]],
+  ['engine_5fenable_5fasserts',['ENGINE_ENABLE_ASSERTS',['../Core_8h.html#a9b3ffcf9f4871788acdf12794eca48cf',1,'Core.h']]]
 ];
